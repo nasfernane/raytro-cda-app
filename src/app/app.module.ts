9d57b6bca65d97d0018315f986ae8cc25ba42bdf
nasfernane <nasfernane@gmail.com>
2021-05-12 12:20:10 +0200

    ajout composants feedback list et feedback container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FeedbackContainerComponent } from './feedbacks/feedback-container/feedback-container.component';
+import { FeedbackItemComponent } from './feedbacks/feedback-item/feedback-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FeedbackContainerComponent, FeedbackItemComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
