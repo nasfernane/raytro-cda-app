61325bd71bb4c2004519d210a9efd2d274ef9cee
nasfernane <nasfernane@gmail.com>
2021-05-15 22:29:13 +0200

    simulation co/devo deuxieme phase

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent implements OnInit {
+  constructor(private AuthService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onLogin() {
+    this.AuthService.login();
+    console.log('Connexion');
   }
-
 }
